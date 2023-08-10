b9c24f79a7fc6a2adb67adcd52a7c0409a113d2d
bee-99 <bee.chokoe@gmail.com>
2023-08-10 12:45:48 -0700

    C program that prints string (char) using puts with return 0 (Task 4)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * This prints string using puts
+ * End value must be zero
+ */
+
+int main(void)
+{
+	char puzzle[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(puzzle);
+
+	return (0);
+}
