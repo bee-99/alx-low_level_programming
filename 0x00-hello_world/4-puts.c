c1524a6381c5e498b978f5d795c5718a260df466
bee-99 <bee.chokoe@gmail.com>
2023-08-10 13:38:02 -0700

    Comment and betty and main description showing me flames (Task 4)

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-/*
- * This prints string using puts
- * End value must be zero
- * Main funtion is what every program begins with
+/**
+ * main - entry point and prints line using puts function
+ * return: 0 (Success)
  */
+
 int main(void)
 {
 	char puzzle[] = "\"Programming is like building a multilingual puzzle";
