f253ffb34bf07be0390661a28feca9d47411bf54
bee-99 <bee.chokoe@gmail.com>
2023-08-11 16:57:06 -0700

    Prints all single digits of base 10 from 0 (Task 5)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - printing base 10 using print f function
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	int t = 0;
+
+	for (t = 0; t <= 9; t++)
+		printf("%d", t);
+
+	printf("\n");
+
+	return (0);
+}
