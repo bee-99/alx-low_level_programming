87c6fa7d9b0cac822e59e0fa41f3539e55546f0e
bee-99 <bee.chokoe@gmail.com>
2023-08-11 12:43:14 -0700

    Printing alphabets using putchar (Task 2)

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - printing in lowercase using putchar function
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	char az = 'a';
+
+	for (az = 'a'; az >= 'z'; az++);
+
+	putchar(az);
+
+	return (0);
+}
