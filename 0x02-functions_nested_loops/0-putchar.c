e7d0df3af5c8dd92dc7595e12a1bfc9de7b21442
bee-99 <bee.chokoe@gmail.com>
2023-08-15 16:15:38 -0700

    Needed fix betty (Task 0)

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,7 +7,7 @@
 
 int main(void)
 {
-        _putchar('_');
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
@@ -17,6 +17,5 @@ int main(void)
 	_putchar('r');
 
 	_putchar('\n');
-
-        return (0);
+	return (0);
 }
