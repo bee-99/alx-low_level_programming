5603878353756dfde769cb3a59f62aa638c32a2a
bee-99 <bee.chokoe@gmail.com>
2023-08-16 12:25:06 -0700

    Computing is return and not print (task 6)

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - function that calculates absolute value of interger
+ * @v: parameter name to print return value
+ * Return: v success
+ */
+
+int _abs(int v)
+{
+	int y = (-1) * v;
+
+	if (v >= 0)
+	{
+		return (v);
+	}
+
+	else
+	{
+		return (y);
+	}
+}
+
