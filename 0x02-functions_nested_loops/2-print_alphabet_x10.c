003d3e4b9cfda711f0c3fa2ceb21337c18ea2f68
bee-99 <bee.chokoe@gmail.com>
2023-08-15 17:48:18 -0700

    Printing alphabet 10 times using nested loop, attempt 2 reducing numbers (Task 2)

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	char pa10 = 'a';
 	int ap10 = 0;
 
-	for (ap10 = 0; ap10 <= 10; ap10++)
+	for (ap10 = 0; ap10 < 10; ap10++)
 	{
 		for (pa10 = 'a'; pa10 <= 'z'; pa10++)
 			_putchar(pa10);
