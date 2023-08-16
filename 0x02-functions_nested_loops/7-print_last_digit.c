09f3ce8437dab2ed05d604d877c97b1f7872b1bd
bee-99 <bee.chokoe@gmail.com>
2023-08-16 14:15:00 -0700

    Printing last letter (Task 7)

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of number using putchar
+ * @l: parameter that calls to print
+ * Return: l Success
+ */
+
+int print_last_digit(int l)
+{
+
+	int n = l % 10;
+
+	if (n >= 0)
+	{
+		_putchar(n + '0');
+	}
+
+	else if (n < 48)
+	{
+		int o = (-1) * n;
+			_putchar(o + '0');
+	}
+
+	return (l);
+}
