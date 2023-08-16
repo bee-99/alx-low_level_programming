d941edb30170d41248824684144bbc48acab9657
bee-99 <bee.chokoe@gmail.com>
2023-08-15 17:46:03 -0700

    Printing alphabet 10 times using nested loop (Task 2)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Printing alphabets x10 using own header
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char pa10 = 'a';
+	int ap10 = 0;
+
+	for (ap10 = 0; ap10 <= 10; ap10++)
+	{
+		for (pa10 = 'a'; pa10 <= 'z'; pa10++)
+			_putchar(pa10);
+		_putchar('\n');
+	}
+}
