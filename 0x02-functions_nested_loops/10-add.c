42ef59ff1d701502479c46ab7007400f713b5d07
bee-99 <bee.chokoe@gmail.com>
2023-08-16 15:47:51 -0700

    Adding numbers and using blank if (task 10)

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * add - adds two intergers with result
+ *
+ * Return: 0 succes
+ */
+
+int add(int t, int b)
+{
+	int p = t + b;
+
+	if (p == 98)
+	{
+
+	}
+	else
+	{	
+	_putchar(p);
+	}
+
+	return(p);
+}
