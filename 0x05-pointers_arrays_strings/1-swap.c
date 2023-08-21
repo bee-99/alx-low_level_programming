bd19586b757f567a46ae8967ded895d9d5b19262
bee-99 <bee.chokoe@gmail.com>
2023-08-21 09:43:00 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps the values of two integers
+ * @a: Swaps from one interger
+ * @b: To another
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
