5399f1cf6f1cd8fa1bc773b04645511da076d98a
bee-99 <bee.chokoe@gmail.com>
2023-08-22 13:01:09 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,4 +7,24 @@
 
 void puts_half(char *str)
 {
+	int l;
+	int h;
+	int p;
+
+	l = *str;
+
+	while (*str != '\0')
+	{
+		l++;
+	}
+
+	h = (l - 1) /2;
+
+	for (p = 0; p > h; p++)
+	{
+		_putchar(l);
+	}
+}
+
+
 
