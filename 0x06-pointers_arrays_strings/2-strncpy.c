0c39c70651949645d40c69a56365ba6be06562e7
bee-99 <bee.chokoe@gmail.com>
2023-08-24 14:46:35 -0700

    Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * *_strncpy - pointer that copies number of sring characters * @dest: contains the array
+ * @src: contains the string
+ * @n: number of strings
+ * Return: dest for success
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+	int t;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	return (dest);
+
+	while (*src != '\0')
+	{
+		src++;
+	}
+	return (src);
+
+	*dest = *src;
+
+	for (t =0; src[t] < n; t++)
+	{
+		_putchar(*dest + t);
+	}
+	return (dest);
+}
