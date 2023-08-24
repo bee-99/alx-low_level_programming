5f9127628aee11d027a364c2c55800797163c1b9
bee-99 <bee.chokoe@gmail.com>
2023-08-24 08:16:57 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * *_strcat - pointer that concentrates two strings
+ * @dest: contains the array
+ * @src: contains the string
+ * Returns char for success
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	int c = 0;
+
+	dest = s2;
+	src = s1;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	return(dest);
+
+	while (*src != '\0')
+	{
+		_putchar(*src);
+		continue;
+	}
+	return(src);
+
+	for (c = 0; dest[c] == '\0' && src[c] == '\0'; c++)
+	{
+		_putchar(*src+c);
+		_putchar(*dest+c);
+	}
+	return(dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+
+#endif
