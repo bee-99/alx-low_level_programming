022ac0a5276648a29da0c49cd350678c552df159
bee-99 <bee.chokoe@gmail.com>
2023-08-24 17:57:35 -0700

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - replaces according to number
+ * @*: relates to char
+ * Return: char
+ */
+
+char *leet(char *l)
+{
+	char z[] = "aeotl";
+	char y[] = "AEOTL";
+	char x[] = "43071";
+
+	l = x;
+
+	if (z != y)
+	{
+		return(l);
+	}
+
+	else if (z == l && y == l)
+	{
+		return(l);
+	}
+
+	return(l);
+}
+
