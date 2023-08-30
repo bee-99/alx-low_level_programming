eb1d4a64767a2193d5018dd1c9044a4da1e1adff
bee-99 <bee.chokoe@gmail.com>
2023-08-30 15:15:35 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: provides the sring
+ * Return: number of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (n != '\0')
+	{
+		n++;
+	}
+
+	else
+	{
+
+	}
+	
+	return(s[n]);
+}
