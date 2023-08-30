abc768a6580050e791fa343f27f6f4083f5c8bf0
bee-99 <bee.chokoe@gmail.com>
2023-08-30 14:55:41 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s: returns the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	char t[100];
+
+	s = t;
+
+	if (t == '\0')
+	{
+		_putchar(*s--);
+	}
+
+	else
+	{
+
+	}
+}
