37d87891ff45cb4203dc081413fadfe91403a2fa
bee-99 <bee.chokoe@gmail.com>
2023-09-07 14:56:39 -0700

    Task 3

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: array
+ * @size: bytes of array
+ * Return: to pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
