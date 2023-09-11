9ad5d9395a8d067e0f0d71c8887bf03b3ae8fd56
bee-99 <bee.chokoe@gmail.com>
2023-09-11 15:04:24 -0700

    Task 0 betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - elements to define
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+
 	struct dog
 	{
 		char *name;
