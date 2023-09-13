c2d1a8d07bcc953370f1e077e3bfe9d3a401e20c
bee-99 <bee.chokoe@gmail.com>
2023-09-13 16:08:04 -0700

    Task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - executes function which is a parameter on arrays
+ * @size: size of array
+ * @action: pointer to function
+ * @array: array
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a;
+
+	while (action == NULL || array == NULL)
+	{
+
+	}
+
+	for (a = 0; a <= size; a++)
+	{
+		action(array[a]);
+	}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTIONPOINTER_H
+#define FUNCTIONPOINTER_H
+
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
