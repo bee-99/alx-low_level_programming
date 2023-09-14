90943ad67e784dca390112b994dc9c8e005c8165
bee-99 <bee.chokoe@gmail.com>
2023-09-14 13:51:31 -0700

    Task 0 betty

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,14 @@ int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int t;
 	int b;
-	
+
 	va_list a;
-	va_start (a,n);
+
+	va_start(a, n);
 
 	for (t = 0; t < n; t++)
 	{
-		b += va_arg (a, int);
+		b += va_arg(a, int);
 	}
 
 	if (n == 0)
@@ -25,7 +26,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	}
 
-	va_end (a);
+	va_end(a);
 
 	return (b);
 }
